00addf092b8303335409f5ea469e58c2058662dd
Mrunali Kale <mrunalikale76@gmail.com>
2023-08-28 00:06:05 +0530

    Add files via upload

---
+++ condition2.java
@@ -0,0 +1,21 @@
+import util.*;
+
+public class condition2 {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+
+        switch(button){
+            case 1: System.out.println("hello");
+            break;
+            case 2: System.out.println("namste");
+            break;
+            case 3: System.out.println("hiii");
+            break;
+            default: System.out.println("inavlid button");
+
+        }
+    }
+    
+}
