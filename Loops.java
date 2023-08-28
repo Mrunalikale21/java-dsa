afaa756c3ade46aad65ebf82cc8a3dc3dbe6cf07
Mrunali Kale <mrunalikale76@gmail.com>
2023-08-28 20:38:18 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,29 @@
+public class Loops {
+
+    public static void main(String args[]){
+        //for(int i=0; i< 11; i++){
+        //    System.out.print(i + " ");
+
+        //int i=0;
+        //while(i< 11){
+          //  System.out.println(i);
+            //i = i + 1;
+            
+         //int i = 0;
+         //do{
+           // System.out.println(i);
+            //i=i+1;
+         //}   while(i<11);
+
+        int i=12;
+        while(i<11){
+            System.out.println("Apna college");
+        } 
+         do{
+            System.out.println("apna college");
+         }while(i<11);
+        }
+        }
+    
+    
+
