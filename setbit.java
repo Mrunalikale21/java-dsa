2c96f7936e6d2d90c8fa56ed3ddcd126e295a2ad
Mrunali Kale <mrunalikale76@gmail.com>
2023-09-18 00:12:40 +0530

    Add files via upload

---
+++ setbit.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class setbit {
+    public static void main(String args[]){
+        int n = 5;
+        int pos = 1;
+        int bitMask = 1<<pos;
+
+        if((bitMask | n) == 0){
+            System.out.print("bit was zero");
+        }
+        else{
+            System.out.println("bit was one");
+        }
+
+        int newNumber = bitMask | n;
+        System.out.println(newNumber);
+    }
+    
+}
