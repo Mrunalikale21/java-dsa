623307344652d97b8b6724ae7ba9dbb18f48f240
Mrunali Kale <mrunalikale76@gmail.com>
2023-10-23 13:53:01 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class fibonacci {
+    public static void printSeries(int n){
+        int series = 0;
+        for(int a=0; a<=n; a++){
+            series = (a-2) + (a-1)
+        }
+    }
+    
+}
