8a4c8713b080dc033ccad814e303b4ce73a07462
Mrunali Kale <mrunalikale76@gmail.com>
2023-10-25 01:18:13 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class dowhile {
+    public static void main(String args[]){
+      do{
+
+        //it will run infinite as no given condition so it will take it as true
+      } while(true) ;
+    }
+}
