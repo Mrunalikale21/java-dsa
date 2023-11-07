07104bcb9ba9eaf3a0aeab5ee01ab845b010b8b3
Mrunali Kale <mrunalikale76@gmail.com>
2023-11-08 01:19:04 +0530

    Add files via upload

---
+++ inverthalfpyrnum.java
@@ -0,0 +1,23 @@
+public class inverthalfpyrnum {
+    public static void main(String args[]){
+        int n=5;
+        //outer loop
+       // for(int i=1; i<=n; i++){
+            //inner loop
+           // for(int j=1; j<=n-i+1; j++){
+               // System.out.print(j + " ");
+           // }
+           // System.out.println();
+         
+         //outer loop
+         for(int i=n; i>=1; i--){
+            //inner loop
+            for(int j=1; j<=i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();      
+         }  
+        
+        }
+    }
+
