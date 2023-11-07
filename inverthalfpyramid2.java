41423e089975dd16a4d37635996041088e7796f2
Mrunali Kale <mrunalikale76@gmail.com>
2023-11-07 20:26:23 +0530

    Add files via upload

---
+++ inverthalfpyramid2.java
@@ -0,0 +1,17 @@
+public class inverthalfpyramid2 {
+ public static void main (String args[]){
+    int n= 4;
+    //outer loop
+    for(int i=1; i<=n; i++){
+        //inner loop for space
+        for(int j=1; j<=n-i; j++){
+        System.out.print(" ");
+        }
+        //inner loop for stars
+        for( int j=1; j<=i; j++){
+            System.out.print("*");
+        } 
+        System.out.println();
+    }
+ }  
+}
