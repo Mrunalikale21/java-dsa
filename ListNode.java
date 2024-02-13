b8259055e7fc79ce47373049a0a8f1fbb7cacca1
Mrunali Kale <mrunalikale76@gmail.com>
2024-02-13 23:43:03 +0530

    Add files via upload

---
+++ ListNode.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class ListNode {
+  ListNode head;
+  ListNode next;
+  int val;
+
+  ListNode(){
+    this.val = val;
+    this.next = next;
+  }
+
+
+
+public ListNode removeNthFromEnd(ListNode head, int n){
+  if(head.next == null){
+    return null;
+  }
+
+  //size
+  int size= 0;
+  ListNode curr = head;
+  while(curr != null){
+    curr = curr.next;
+    size++;
+  }
+
+  if(n == size){
+    return head;
+  }
+
+  int indexToSearch = size - n ;
+  ListNode prev = head;
+  int i = 1;
+  while(i < indexToSearch){
+    prev = prev.next;
+    i++;
+  }
+
+  prev.next = prev.next.next;
+  return head;
+}
+}
+
